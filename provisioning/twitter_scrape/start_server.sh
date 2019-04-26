45cd83f51c2b3780552a4ee74799a475c5d3b15d
Artem Nepo <artem@anymindgroup.com>
2019-04-26 17:26:28 +0700

    Update provisioning

--- provisioning/twitter_scrape/start_server.sh
+++ provisioning/twitter_scrape/start_server.sh
@@ -17,11 +17,11 @@ if [ $DEBUG -eq 1 ]; then
 # for more info see: http://honcho.readthedocs.org/en/latest/using_procfiles.html#buffered-output
 echo "PYTHONUNBUFFERED=true" >> /mnt/.env
 cat >Procfile <<EOM
-server: gunicorn  --worker-class=gthread --threads=4 --graceful-timeout 1500 --timeout 2000 --bind=0.0.0.0:8888 --log-level=debug --log-file=/var/log/out.log  --access-logfile=/var/log/access.log main:app 2>>/var/log/out.log
+server: gunicorn  --worker-class=gthread --threads=4 --graceful-timeout 1500 --timeout 2000 --bind=0.0.0.0:8888 --log-level=debug --log-file=/var/log/out.log  --access-logfile=/var/log/access.log app:app 2>>/var/log/out.log
 EOM
 else
 cat >Procfile <<EOM
-server: gunicorn --worker-class=gthread --threads=4 --graceful-timeout 1500 --timeout 2000 --bind=0.0.0.0:8888 --log-level=info --log-file=/var/log/out.log  --access-logfile=/var/log/access.log main:app 2>>/var/log/out.log
+server: gunicorn --worker-class=gthread --threads=4 --graceful-timeout 1500 --timeout 2000 --bind=0.0.0.0:8888 --log-level=info --log-file=/var/log/out.log  --access-logfile=/var/log/access.log app:app 2>>/var/log/out.log
 EOM
 fi
 
